94bdb0876fbc0aca7309aae20e8c53904ce0ada7
dxr3977 <dxr3977@g.rit.edu>
2016-06-30 16:04:39 -0400

    Initial commit

---
+++ src/CubeSolver.java
@@ -0,0 +1,33 @@
+
+import java.io.FileNotFoundException;
+import java.util.Optional;
+
+/**
+ * Created by Lord Daniel on 5/28/2016.
+ */
+public class CubeSolver {
+    public static void main(String[] args) throws FileNotFoundException {
+
+            // construct the initial configuration from the file
+            Configuration init = new CubeConfig(args[0]);
+
+            Backtracker bt = new Backtracker(false);
+
+            // start the clock
+            double start = System.currentTimeMillis();
+
+            // attempt to solve the puzzle
+            Optional<Configuration> sol = bt.solve(init);
+
+            // compute the elapsed time
+            System.out.println("Elapsed time: " +
+                    (System.currentTimeMillis() - start)/1000.0 + " seconds.");
+
+            // indicate whether there was a solution, or not
+            if (sol.isPresent()) {
+                System.out.println("SOLVED");
+            } else {
+                System.out.println("No solution!");
+            }
+    }
+}
